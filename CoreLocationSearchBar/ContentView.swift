a3592d894b7f8bab1bf973303fe86a3966c894ec
Max <maxim.macari.dam@gmail.com>
2021-01-03 18:14:51 +0100

    Initial Commit

---
+++ CoreLocationSearchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreLocationSearchBar
+//
+//  Created by Maxim Macari on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreLocationSearchBar/CoreLocationSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationSearchBarApp.swift
+//  CoreLocationSearchBar
+//
+//  Created by Maxim Macari on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
